e39c9303df45925f632df60ddc4655a9c341ff13
leefuyong <tingting521>
2021-01-18 21:58:00 +0800

    c

---
+++ fastdevelopinjava-service-ucenter/src/main/java/com/fastdevelopinjava/service/ucenter/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.fastdevelopinjava.service.ucenter.mapper;
+
+import com.fastdevelopinjava.framework.api.ucenter.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
